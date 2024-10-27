c7d35fd6b4c22ca424236ae7d0f751b8f8867ea4
Moon <1003844689@qq.com>
2024-10-27 17:41:35 +0800

    feat: add Novel.msg

---
+++ town_ws/build/village_interfaces/CMakeFiles/village_interfaces__rosidl_generator_c.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/village_interfaces__rosidl_generator_c.dir/rosidl_generator_c/village_interfaces/msg/detail/novel__functions.c.o"
+  "CMakeFiles/village_interfaces__rosidl_generator_c.dir/rosidl_generator_c/village_interfaces/msg/detail/novel__functions.c.o.d"
+  "libvillage_interfaces__rosidl_generator_c.pdb"
+  "libvillage_interfaces__rosidl_generator_c.so"
+  "rosidl_generator_c/village_interfaces/msg/detail/novel__functions.c"
+  "rosidl_generator_c/village_interfaces/msg/detail/novel__functions.h"
+  "rosidl_generator_c/village_interfaces/msg/detail/novel__struct.h"
+  "rosidl_generator_c/village_interfaces/msg/detail/novel__type_support.h"
+  "rosidl_generator_c/village_interfaces/msg/novel.h"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/village_interfaces__rosidl_generator_c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ town_ws/build/village_interfaces/village_interfaces__py/CMakeFiles/village_interfaces__py.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "../rosidl_generator_py/village_interfaces/_village_interfaces_s.ep.rosidl_typesupport_c.c"
+  "../rosidl_generator_py/village_interfaces/_village_interfaces_s.ep.rosidl_typesupport_fastrtps_c.c"
+  "../rosidl_generator_py/village_interfaces/_village_interfaces_s.ep.rosidl_typesupport_introspection_c.c"
+  "../rosidl_generator_py/village_interfaces/msg/__init__.py"
+  "../rosidl_generator_py/village_interfaces/msg/_novel.py"
+  "../rosidl_generator_py/village_interfaces/msg/_novel_s.c"
+  "CMakeFiles/village_interfaces__py"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/village_interfaces__py.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
